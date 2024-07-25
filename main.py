d353b6d910b57e7d1a3b07913a051a80e6a4bc5f
David Flores <david.flores@itsoft.mx>
2024-07-25 14:25:04 -0600

    feat: Aplicar el modelo a la Lista y devolver a lista en Post

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from pydantic import BaseModel, Field
-from typing import Union, Optional
+from typing import List, Union, Optional
 from fastapi import FastAPI, HTTPException, Body
 
 app = FastAPI()
 app.title = "API con FastAPI"
-
-TODO_LIST = [
-        { "id": 1, "description" : "Mi fist Task", "completed" : True },
-        { "id": 2, "description" : "Learn Python", "completed" : False },
-        { "id": 3, "description" : "Learn FastAPI", "completed" : False },
-    ]
 
 #Definir el modelo TODO
 class Todo(BaseModel):
     id: Optional[int] = None
     description: str = Field(min_length=3, max_length=255)
     completed: bool = Field(default=False)
+
+
+TODO_LIST: List[Todo] = [
+        { "id": 1, "description" : "Mi fist Task", "completed" : True },
+        { "id": 2, "description" : "Learn Python", "completed" : False },
+        { "id": 3, "description" : "Learn FastAPI", "completed" : False },
+    ]
 
 @app.get("/")
 async def hello_word() :
@@ -32,7 +33,6 @@ async def get_all( completed: Union[ bool, None] =  None) :
     
 @app.get("/todo/{todo_id}")
 async def get_todo( todo_id: int ):
-
     try:
         todo_data = next( todo for todo in TODO_LIST if todo["id"] == todo_id )
         return todo_data
@@ -40,10 +40,15 @@ async def get_todo( todo_id: int ):
         raise HTTPException(status_code= 404, detail= "TODO Not Found")
 
 
-@app.post("/todo")
+@app.post("/todo", response_model=List[Todo], name="Create new TODO", 
+          summary="Create new TODO item",
+          description="Create a new TODO given a description and a completed boolean value",
+          status_code=201)
 async def add_todo( data: Todo ):
-    TODO_LIST.append(Todo)
+    TODO_LIST.append(data)
     return TODO_LIST
+
+
 
 
 
